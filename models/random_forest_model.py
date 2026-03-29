0ddcd63c0b0fe979b745a9e5d741d4d0c0fccbb2
jak2002 <jacktonmulongo8@gmail.com>
2026-03-29 22:35:42 +0300

    Describe the change you made, e.g. Remove RQ code and restore predict route

--- models/random_forest_model.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-class RandomForestModel:
-    def __init__(self):
-        self.model = joblib.load("models/random_forest.pkl")
-
-    def predict(self, data):
-        return self.model.predict([data])[0]
--- models/svm_model.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-class SVMModel:
-    def __init__(self):
-        self.model = joblib.load("models/svm.pkl")
-
-    def predict(self, data):
-        return self.model.predict([data])[0]
